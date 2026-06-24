3b4e4e2ae0a7ef99dd6e5cb692dceafc17cea161
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2026-06-24 10:59:52 +0200

    modification arboressence

--- src/index.php
+++ src/index.php
@@ -1,7 +1,9 @@
 <?php
-echo "<h1>Mon serveur PHP fonctionne !</h1>";
+//echo "<h1>Mon serveur PHP fonctionne !</h1>";
 
 // Vérification des drivers PDO installés
-echo "<h2>Drivers PDO disponibles :</h2>";
-print_r(PDO::getAvailableDrivers());
-?>
+//echo "<h2>Drivers PDO disponibles :</h2>";
+//print_r(PDO::getAvailableDrivers());
+//sleep(5);
+header("Location: vue/F1.html");
+exit();
