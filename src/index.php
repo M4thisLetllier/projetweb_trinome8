dc25dd68bd352b76e39111ae288fa438e91f8180
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2026-06-26 08:38:30 +0200

    enlevement ligne temporaire

--- src/index.php
+++ src/index.php
@@ -1,9 +1,3 @@
 <?php
-//echo "<h1>Mon serveur PHP fonctionne !</h1>";
-
-// Vérification des drivers PDO installés
-//echo "<h2>Drivers PDO disponibles :</h2>";
-//print_r(PDO::getAvailableDrivers());
-//sleep(5);
 header("Location: vue/Page1Accueil.html");
 exit();
